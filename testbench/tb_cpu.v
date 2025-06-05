b82f77bd056dea02f3a481cf5200a6ade87481b5
lollo004 <info.borghilorenzo@gmail.com>
2025-06-05 16:25:28 +0200

    testbench display status

--- testbench/tb_cpu.v
+++ testbench/tb_cpu.v
@@ -7,16 +7,19 @@ module tb_cpu;
     always #5 clk = ~clk;  // 10ns clock period
 
     always @(posedge clk) begin
-        $display("r2=%d, r3=%d, r4=%d", 
+        $display("PC=%d | r2=%d | r3=%d | r4=%d | r5=%d | r6=%d", 
+            uut.PC,
             uut.regfile_inst.registers[2],
             uut.regfile_inst.registers[3],
-            uut.regfile_inst.registers[4]);
+            uut.regfile_inst.registers[4],
+            uut.regfile_inst.registers[5],
+            uut.regfile_inst.registers[6]);
         end
             
     initial begin
         $dumpfile("cpu.vcd");
         $dumpvars(0, tb_cpu);
         #5 rst = 0;       // Release reset after 10ns
-        #200 $finish;
+        #300 $finish;
     end
 endmodule
