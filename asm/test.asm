53c81efe4fb1cec19b2f786f175e7cfa3078533c
lollo004 <info.borghilorenzo@gmail.com>
2025-06-05 20:28:55 +0200

    for example program

---
+++ asm/test.asm
@@ -0,0 +1,19 @@
+.data
+
+    n: 20
+    i: 2
+    
+
+.text
+Start:
+    lw n
+    add %r0 %r2 %r3
+    lw i
+    add %r0 %r2 %r5
+    and %r0 %r0 %r4
+    
+Loop:
+    add %r4 %r5 %r4
+    sub %r3 %r4 %r6
+    beq Start
+    jmp Loop
